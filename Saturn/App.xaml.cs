af3ed9879dc4075fd00701a6d12d0501365ca575
Kanat9494 <kkanat94@gmail.com>
2024-01-05 15:55:24 +0600

    Add project files.

---
+++ Saturn/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Saturn
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Saturn/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Saturn
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
