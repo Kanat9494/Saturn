79e7edc83e8f1bb91d0df9d62ae305698ba39e0d
kana2022 <kkanat94@gmail.com>
2024-02-16 14:26:10 +0600

    1.0.56

--- Saturn/Views/Chat/ChatPage.xaml.cs
+++ Saturn/Views/Chat/ChatPage.xaml.cs
@@ -26,13 +26,4 @@ public partial class ChatPage : ContentPage
 
 		_viewModel.OnDisappearing();
 	}
-
-	void ClearNavigationStack()
-	{
-        var stack = Shell.Current.Navigation.NavigationStack.ToArray();
-        for (int i = stack.Length - 1; i > 0; i--)
-        {
-            Shell.Current.Navigation.RemovePage(stack[i]);
-        }
-    }
 }
