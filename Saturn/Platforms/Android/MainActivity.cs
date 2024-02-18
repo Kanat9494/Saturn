034102c3ab1412d8b1304555ab20f77854ed33e9
Kanat9494 <kkanat94@gmail.com>
2024-02-18 20:45:09 +0600

    1.0.72

--- Saturn/Platforms/Android/MainActivity.cs
+++ Saturn/Platforms/Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.OS;
 
 namespace Saturn
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTask, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     [IntentFilter(new[] {Android.Content.Intent.ActionView}, 
         DataScheme = "https", 
         DataHost = "letoinc.saturn.kg", 
