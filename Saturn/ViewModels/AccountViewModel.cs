4aecb3910fc1b9ceef3addcb7103b2429de23bdd
kana2022 <kkanat94@gmail.com>
2024-02-19 15:44:55 +0600

    1.0.96

--- Saturn/ViewModels/AccountViewModel.cs
+++ Saturn/ViewModels/AccountViewModel.cs
@@ -16,12 +16,6 @@ internal class AccountViewModel : BaseViewModel
         get => _currentUser;
         set => SetProperty(ref _currentUser, value);
     }
-    private string _imageUrl;
-    public string ImageUrl
-    {
-        get => _imageUrl;
-        set => SetProperty(ref _imageUrl, value);
-    }
     
 
     private async Task OnSignOut()
@@ -39,8 +33,5 @@ internal class AccountViewModel : BaseViewModel
             UserName = "Кудайбергенов Канат Кудайбергенович",
             ProfileImageSource = "https://picsum.photos/id/301/200/300"
         };
-
-        ImageUrl = null;
-        ImageUrl = "https://picsum.photos/id/301/200/300";
     }
 }
